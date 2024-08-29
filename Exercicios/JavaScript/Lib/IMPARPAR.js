2bc36aa00a6c0b37a3f4af29615d04aee1fa9422
MUCANDA <mucanda@discente.ufg.br>
2024-08-29 14:30:11 -0300

    resulucao do exercicio da lista 4

--- Exercicios/JavaScript/Lib/IMPARPAR.js
+++ Exercicios/JavaScript/Lib/IMPARPAR.js
@@ -1,14 +1,9 @@
-function par(a) {
-let numero = a.filter(value => value % 2 == 0);
 
-let total = 0;
+const par = require("./modulo");
 
-for(let i=0; i<numero.length; i++) {
-    total += numero[i];
-}
-return total;
-}
 console.log(par([1, 2, 3, 4, 5]));
 console.log(par([2, 4, 6, 8]));
 console.log(par([1, 2, 3, 4, 5]));
 console.log(par([1, 3, 5, 7]));
+console.log(par([0]));
+console.log(par([1]));
