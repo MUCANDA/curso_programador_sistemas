b2b74a5e5745273f52761cb54adc297842e9b668
MUCANDA <mucanda@discente.ufg.br>
2024-09-16 18:25:42 -0300

    Gera tabelas de forma dinamica

--- PROJETO_CSS/sokoban.js
+++ PROJETO_CSS/sokoban.js
@@ -2,22 +2,79 @@ const tabela2 = document.getElementById('tabela2');
 const M = 8;
 const N = 8;
 
-for (let k = 0; k < M; k++) {
-    const linha = document.createElement('div');
-    linha.classList.add('linha');
 
-    for (let i = 0; i < N; i++) {
-        const celula = document.createElement('div');
-        celula.classList.add('celula');
-        linha.appendChild(celula);
 
-        //        if(i == 0 || i==N-1|| k==0 || k==M-1) {
-        //  celula.classList.add('vazio');
+//buildGameBoard(8, 8, rule0);
+buildGameBoard(8, 8, rule1);
+buildGameBoard(8, 8, rule2);
+buildGameBoard(8, 8, rule3);
+buildGameBoard(8, 8, rule4);
+buildGameBoard(8, 8, rule5);
+//buildGameBoard(8, 8, rule6);
+//buildGameBoard(8, 8, rule7);
 
-        if (i > 0 && i < 7 && k > 0 && k < 7) {
-            celula.classList.add('vazio');
+
+
+
+
+
+function buildGameBoard(numRows, numCols, drawrule) {
+    const game = document.getElementById("jogo");
+    const board = document.createElement('div');
+    board.classList.add('tabela');
+
+
+    for (let k = 0; k < numRows; k++) {
+        const linha = document.createElement('div');
+        linha.classList.add('linha');
+        board.append(linha);
+
+        for (let i = 0; i < numCols; i++) {
+            const celula = document.createElement('div');
+            celula.classList.add('celula');
+            linha.appendChild(celula);
+
+            drawrule(celula, numRows, numCols, k, i);
+
+
+
+            //        if(i == 0 || i==N-1|| k==0 || k==M-1) {
+            //  celula.classList.add('vazio');
+
+            // if (i > 0 && i < 7 && k > 0 && k < 7) {
+            //     celula.classList.add('vazio');
 
         }
-        tabela2.append(linha);
     }
+    game.append(board);
+
+}
+
+function rule1(celula, numRows, numCols, k, i) {
+    if (i == 0 || i == N - 1 || k == 0 || k == M - 1) {
+        celula.classList.add('vazio');
+    }
+}
+function rule2(celula, numRows, numCols, k, i) {
+
+    if (k > 0 && i < numRows - 1 && k > 0 && k < numCols - 1) {
+        celula.classList.add('vazio')
+    }
+}
+function rule3(celula, numRows, numCols, k, i) {
+    if (k == i) {
+        celula.classList.add('vazio');
+    }
+
+}
+function rule4(celula, numRows, numCols, k, i) {
+    if (k + i == numCols - 1) {
+        celula.classList.add('vazio')
+    }
+}
+function rule5(celula, numRows, numCols, k, i) {
+    if (k == i || k + i == numCols - 1) {
+        celula.classList.add('vazio')
+    }
+
 }
