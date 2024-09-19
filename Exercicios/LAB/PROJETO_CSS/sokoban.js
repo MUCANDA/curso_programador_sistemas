1a62ab002a854ed687f1f05b0050a27e8e2254ae
MUCANDA <mucanda@discente.ufg.br>
2024-09-19 17:07:22 -0300

    criar a class player

--- Exercicios/LAB/PROJETO_CSS/sokoban.js
+++ Exercicios/LAB/PROJETO_CSS/sokoban.js
@@ -1,36 +1,40 @@
-let x=3;
-let y=4;
+// let x=3;
+// let y=4;
 
+function Player (posx,posy) {
+
+    this.x = posx;
+    this.y= posy;
+   
+}
+   
+const player = new Player(3, 4);   
+   
+console.log(player);  
 
 const vazio = document.querySelector('.vazio');
 console.log(vazio);
 
-vazio.addEventListener("click" , function() {
-    alert("clicou no jogador");
-})
+// vazio.addEventListener("click" , function() {
+//     alert("clicou no jogador");
+// })
 
-window.addEventListener("keydown", function(event){
+
+ window.addEventListener("keydown", function(event){
     // this.window.alert("pressionou tecla");
     nextPosition(event.code);
 
-    console.log(event);
-    console.log(event.code);
+    // console.log(event);
+    // console.log(event.code);
 
-
-   
-       
-     
  })
- function nextPosition(keycode) {
-    if(keycode == "ArrowUp") x--;
-    if (keycode== "ArrowDown") x++;
-     if(keycode == "ArrowLeft") y++;
-     if(keycode == "ArrowrRight") y--;
 
-     console.log(keycode, x, y);
+ function nextPosition(keycode) {
+    if(keycode == "ArrowUp") player.x--;
+    if (keycode== "ArrowDown") player.x++;
+     if(keycode == "ArrowLeft") player.y++;
+     if(keycode == "ArrowrRight") player.y--;
+
+     console.log(keycode, player);
 
  }
-
-
- 
-
