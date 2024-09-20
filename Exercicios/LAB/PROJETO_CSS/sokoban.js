5fe678a75e904290abc8d1620987c2fc811b940f
MUCANDA <mucanda@discente.ufg.br>
2024-09-20 17:43:09 -0300

    fazer o bixinho mover

--- Exercicios/LAB/PROJETO_CSS/sokoban.js
+++ Exercicios/LAB/PROJETO_CSS/sokoban.js
@@ -1,46 +1,60 @@
 // let x=3;
 // let y=4;
 
-function Player (posx,posy) {
 
-    this.x = posx;
-    this.y= posy;
+
+const player = new Player(0, 0);
+const celulas = document.querySelectorAll('.celula');
+const playerElement = document.querySelector('.jogador');
+
+window.addEventListener("keydown", function (event) {
+
+   const next = player.nextPosition(event.code);
+
+   if (verifyPosition(next)) {
+
+      let k = next.x * 8 + next.y;
+      console.log(next);
+      // const { x, y } = next;
+      // const k = x * 8 + y;
    
+
+      player.moveTo(next, playerElement, celulas[k]);
+   }
+})
+
+function Player(posx, posy) {
+
+   this.x = posx;
+   this.y = posy;
+
+   this.nextPosition = function (keycode) {
+      console.log(keycode);
+
+
+      let { x, y } = this;
+      if (keycode == "ArrowUp") x--;
+      if (keycode == "ArrowDown") x++;
+      if (keycode == "ArrowLeft") y--;
+      if (keycode == "ArrowRight") y++;
+
+
+      return { x, y };
+
+   }
+
+   this.moveTo = function (position, element, parent) {
+
+      this.x = position.x;
+      this.y = position.y;
+      parent.append(element);
+
+   }
 }
-   
-const player = new Player(0, 0);   
-   
-console.log(player);  
 
-const vazio = document.querySelector('.vazio');
-console.log(vazio);
+function verifyPosition(position) {
+   let { x, y } = position;
 
-// vazio.addEventListener("click" , function() {
-//     alert("clicou no jogador");
-// })
+   return x >= 0 && x < 8 && y >= 0 && y < 8;
+}
 
-
- window.addEventListener("keydown", function(event){
-    // this.window.alert("pressionou tecla");
-    nextPosition(event.code);
-
-    // console.log(event);
-    // console.log(event.code);
-
- })
- const celulas = document.querySelectorAll('.celula');
-  console.log(celulas);
- function nextPosition(keycode) {
-    if(keycode == "ArrowUp") player.x--;
-    if (keycode== "ArrowDown") player.x++;
-     if(keycode == "ArrowLeft") player.y++;
-     if(keycode == "ArrowrRight") player.y--;
-
-     console.log(keycode, player);
-
-     const k = player.x * 8 + player.y;
-
-celulas[k].append(vazio);
- }
-
- 
